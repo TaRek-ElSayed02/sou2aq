0fa52bbd56316aba383dac1da5b7593e6a95eb59
mohamed <mohashraf359@gmail.com>
2026-01-03 17:01:33 +0200

    update redux auth

--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 
+import {
+  persistStore,
+  persistReducer,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
+
+const persistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["user", "accessToken", "refreshToken"],
+};
+
+const persistedAuthReducer = persistReducer(
+  persistConfig,
+  authReducer
+);
+
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
